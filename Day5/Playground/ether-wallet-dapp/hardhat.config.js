1b53bbf6956638dc3b9ee904f8f69ac9df29d495
DarSahran <darsahran12@gmail.com>
2025-07-25 16:02:33 +0530

    dd

--- Day5/Playground/ether-wallet-dapp/hardhat.config.js
+++ Day5/Playground/ether-wallet-dapp/hardhat.config.js
@@ -1,3 +1,24 @@
+const { task } = require('hardhat/config')
+
+// Define a Hardhat task to print accounts and their balances
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task(
+  'accounts',
+  'Prints the list of accounts and their balances',
+  async (taskArgs, hre) => {
+    // Retrieve all signers from the Hardhat environment
+    const accounts = await hre.ethers.getSigners();
+
+    // Loop through each account and log its balance
+    for (const account of accounts) {
+      // Get the balance of the current account
+      const balance = await hre.ethers.provider.getBalance(account.address);
+      console.log(account.address, ': ', balance, 'ETH');
+    }
+  }
+);
+
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -6,5 +27,6 @@ module.exports = {
   paths: {
     sources: "./contracts",
     artifacts: "./src/artifacts",
-  }
+  },
+
 };
